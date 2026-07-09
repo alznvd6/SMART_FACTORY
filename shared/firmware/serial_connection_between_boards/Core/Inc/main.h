c25e2cf7b8ef154b27f5281bb0335a119bb7a4a5
Ali Reza Navid <alz.navid6@gmail.com>
2026-07-09 16:54:52 +0330

    resolve console UI text scrambling in Proteus
    Switches the local terminal output from raw coordinate cursor positioning
    to clean scrolling newlines, removing corrupted artifact characters.

--- shared/firmware/serial_connection_between_boards/Core/Inc/main.h
+++ shared/firmware/serial_connection_between_boards/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define ACTUATOR_LED_Pin GPIO_PIN_13
+#define ACTUATOR_LED_GPIO_Port GPIOC
 
 /* USER CODE BEGIN Private defines */
 
